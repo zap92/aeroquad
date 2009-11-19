7555e4e5e5d32cd72955af561adfdc3dc986ed83
pyjamasam <pyjamasam@80d9e1a6-1c2b-11de-a108-cd2f117ce590>
2009-11-19 15:13:59 +0000

    Added the "blinkie" example subsystem.  Shows a simple subsystem implementation that just blinks pin 13 Also added a bunch of comments to the subsystem code to detail better how it works.

--- AeroQuad/SubSystem.h
+++ AeroQuad/SubSystem.h
@@ -21,17 +21,25 @@
  along with this program. If not, see <http://www.gnu.org/licenses/>. 
  */
 
+
+/* 
+This is the base SubSystem class.  Its designed purpose is to abstract all the "scheduleing" for an AeroQuad component.
+To use this class you would create a subclass of it and impliment the specific logic for what your building.
+*/
+
 class SubSystem
 {
 private:
-  unsigned int _enabled;
-  unsigned long _lastRunTime;
-
-  unsigned int _frequency;
+  //Private properties used to store the state of a SubSystem
+  unsigned int _enabled;              //Is this SubSystem enabled
+  
+  unsigned long _lastRunTime;         //When was the last time this SubSystem ran (in ms)
+  unsigned int _frequency;            //How often should this SubSystem run (in ms)
 
 public:
   SubSystem() 
   {
+    //Init all the private properties
     _enabled = 0;
     _lastRunTime = 0;
     _frequency = 0;
@@ -39,15 +47,24 @@ public:
 
   void _initialize(unsigned int frequency, unsigned int offset = 0)
   {
+    //Assign the properties to for this SubSystem
     _frequency = frequency;
     _lastRunTime = offset;
+    
+    //Ensure that this SubSystem is marked as enabled
     _enabled = 1;
   }
-  virtual void initialize(unsigned int frequency, unsigned int offset) { 
+  
+  //This method can be overloaded by subclasses to perform any necessary initalization.
+  //It is imporatnt for any overloaded methods to call the _initialize method to ensure correct setup
+  //of a SubSystem
+  virtual void initialize(unsigned int frequency, unsigned int offset) 
+  { 
     this->_initialize(frequency, offset); 
   }
 
-  void enable()
+  //These two methods allow a SubSystem to be enabled or disabled at runtime.  
+  void enable()  
   {
     _enabled = 1;
   }
@@ -57,11 +74,15 @@ public:
     _enabled = 0;
   }
 
+  //This method lets the current state of this SubSystem be queried.
   unsigned int enabled()
   {
     return _enabled;
   }
 
+  //This method should be called from the overloaded process method listed below.
+  //Based on it's return value the subclassed SubSystem can decided to continue processing or stop and wait for the next cycle.
+  //It's generally called inside an if and is the first thing done inside the process method.
   unsigned int _canProcess(unsigned long currentTime)
   {
     if (_enabled == 1)
@@ -76,8 +97,10 @@ public:
     return 0;
   }
 
-  virtual void process(unsigned long currentTime) {
-  }
+  //This method is ment to be overloaded in the subclass.  It will perform all processing related to the SubSystem
+  //it can be though of as the main loop for the SubSystem
+  //It should call _canProcess to check to see if the SubSystem is enabled and enough time has elapsed since the last processing cycle
+  virtual void process(unsigned long currentTime) {}
 };
 
 #endif
