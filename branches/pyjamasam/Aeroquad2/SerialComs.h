f4677f1822a8e970606ef8d92da8d0f2cefc7290
pyjamasam <pyjamasam@80d9e1a6-1c2b-11de-a108-cd2f117ce590>
2010-08-16 23:30:17 +0000

    working on making changes to the serial coms stuff to start work on a ground station of sorts

--- branches/pyjamasam/Aeroquad2/SerialComs.h
+++ branches/pyjamasam/Aeroquad2/SerialComs.h
@@ -5,12 +5,17 @@
 #define BAUD 115200
 #define MAXASSIGNEDSERIALPORTS 2
 
+class FastSerialPort : public HardwareSerial
+{
+	
+};
+
 class SerialComs : public SubSystem
 {
 	private:
 		struct serialPortInformation
 		{
-			HardwareSerial *serialPort;
+			FastSerialPort *serialPort;
 			
 			bool debugPort;
 			
@@ -22,123 +27,6 @@ class SerialComs : public SubSystem
 		
   		unsigned int _serialPortCount;
 		serialPortInformation	_assignedSerialPorts[MAXASSIGNEDSERIALPORTS];
-		
-		void printNumber(unsigned long n, uint8_t base)
-		{
-		 	unsigned char buf[8 * sizeof(long)]; // Assumes 8-bit chars. 
-			unsigned long i = 0;
-
-			if (n == 0) 
-			{
-				print('0');
-				return;
-			} 
-
-			while (n > 0) 
-			{
-				buf[i++] = n % base;
-				n /= base;
-			}
-
-			for (; i > 0; i--)
-			{
-				print((char) (buf[i - 1] < 10 ? '0' + buf[i - 1] : 'A' + buf[i - 1] - 10));
-			}
-		}
-	
-	
-		void debugPrintNumber(unsigned long n, uint8_t base)
-		{
-		 	unsigned char buf[8 * sizeof(long)]; // Assumes 8-bit chars. 
-			unsigned long i = 0;
-
-			if (n == 0) 
-			{
-				debugPrint('0');
-				return;
-			} 
-
-			while (n > 0) 
-			{
-				buf[i++] = n % base;
-				n /= base;
-			}
-
-			for (; i > 0; i--)
-			{
-				debugPrint((char) (buf[i - 1] < 10 ? '0' + buf[i - 1] : 'A' + buf[i - 1] - 10));
-			}
-		}
-
-		void printFloat(double number, uint8_t digits) 
-		{ 
-			// Handle negative numbers
-		  	if (number < 0.0)
-		  	{
-		     	print('-');
-		     	number = -number;
-		  	}
-
-		  	// Round correctly so that print(1.999, 2) prints as "2.00"
-		  	double rounding = 0.5;
-		  	for (uint8_t i=0; i<digits; ++i)
-		    	rounding /= 10.0;
-
-		  	number += rounding;
-
-		  	// Extract the integer part of the number and print it
-			unsigned long int_part = (unsigned long)number;
-			double remainder = number - (double)int_part;
-			print(int_part);
-
-		  	// Print the decimal point, but only if there are digits beyond
-		  	if (digits > 0)
-		    	print("."); 
-
-		  	// Extract digits from the remainder one at a time
-		  	while (digits-- > 0)
-		  	{
-		    	remainder *= 10.0;
-		    	int toPrint = int(remainder);
-		    	print(toPrint);
-		    	remainder -= toPrint; 
-		  	} 
-		}
-		
-		void debugPrintFloat(double number, uint8_t digits) 
-		{ 
-			// Handle negative numbers
-		  	if (number < 0.0)
-		  	{
-		     	debugPrint('-');
-		     	number = -number;
-		  	}
-
-		  	// Round correctly so that print(1.999, 2) prints as "2.00"
-		  	double rounding = 0.5;
-		  	for (uint8_t i=0; i<digits; ++i)
-		    	rounding /= 10.0;
-
-		  	number += rounding;
-
-		  	// Extract the integer part of the number and print it
-			unsigned long int_part = (unsigned long)number;
-			double remainder = number - (double)int_part;
-			debugPrint(int_part);
-
-		  	// Print the decimal point, but only if there are digits beyond
-		  	if (digits > 0)
-		    	debugPrint("."); 
-
-		  	// Extract digits from the remainder one at a time
-		  	while (digits-- > 0)
-		  	{
-		    	remainder *= 10.0;
-		    	int toPrint = int(remainder);
-		    	debugPrint(toPrint);
-		    	remainder -= toPrint; 
-		  	} 
-		}
 
 	public:
 		
@@ -199,170 +87,6 @@ class SerialComs : public SubSystem
 				}
 			}
   		}
-
-		//Overloaded from Print
-		void write(uint8_t c)
-		{
-			for (unsigned int i = 0; i < _serialPortCount; i++)
-			{
-				_assignedSerialPorts[i].serialPort->write(c);
-			}
-		}
-	    void write(const char *str)
-		{
-	 		while (*str)
-				write(*str++);
-		}
-	    void write(const uint8_t *buffer, size_t size)
-		{
-			while (size--)
-		    	write(*buffer++);
-		}
-
-		void debugWrite(uint8_t c)
-		{
-			for (unsigned int i = 0; i < _serialPortCount; i++)
-			{
-				if (_assignedSerialPorts[i].debugPort)
-				{
-					_assignedSerialPorts[i].serialPort->write(c);
-				}
-			}
-		}
-	    void debugWrite(const char *str)
-		{
-	 		while (*str)
-				debugWrite(*str++);
-		}
-	    void debugWrite(const uint8_t *buffer, size_t size)
-		{
-			while (size--)
-		    	debugWrite(*buffer++);
-		}
-		
-		void print(const char str[])
-		{
-			write(str);
-		}
-	    void print(char c, int base = BYTE)
-		{
-			print((long) c, base);
-		}
-	    void print(unsigned char b , int base = BYTE)
-		{
-			print((unsigned long) b, base);
-		}
-	    void print(int n, int base = DEC)
-		{
-			print((long) n, base);	
-		}
-	    void print(unsigned int n, int base = DEC)
-		{
-			print((unsigned long) n, base);	
-		}
-	    void print(long n, int base = DEC)
-		{
-			if (base == 0) 
-			{
-		    	write(n);
-		  	}
-		 	else if (base == 10) 
-			{
-		    	if (n < 0) 
-				{
-		    		print('-');
-		      		n = -n;
-		    	}
-		    	printNumber(n, 10);
-		  	} 
-			else 
-			{
-		    	printNumber(n, base);
-		  	}
-		}
-	    void print(unsigned long n, int base = DEC)
-		{
-			if (base == 0) 
-			{
-				write(n);
-			}
-		  	else 
-			{
-				printNumber(n, base);
-			}
-		}
-	    void print(double n, int digits = 2)
-		{
-			printFloat(n, digits);
-		}
-		void println(void)
-		{
-			print('\r');
-			print('\n');  
-		}
-		
-		void debugPrint(const char str[])
-		{
-			debugWrite(str);
-		}
-	    void debugPrint(char c, int base = BYTE)
-		{
-			debugPrint((long) c, base);
-		}
-	    void debugPrint(unsigned char b , int base = BYTE)
-		{
-			debugPrint((unsigned long) b, base);
-		}
-	    void debugPrint(int n, int base = DEC)
-		{
-			debugPrint((long) n, base);	
-		}
-	    void debugPrint(unsigned int n, int base = DEC)
-		{
-			debugPrint((unsigned long) n, base);	
-		}
-	    void debugPrint(long n, int base = DEC)
-		{
-			if (base == 0) 
-			{
-		    	debugWrite(n);
-		  	}
-		 	else if (base == 10) 
-			{
-		    	if (n < 0) 
-				{
-		    		print('-');
-		      		n = -n;
-		    	}
-		    	debugPrintNumber(n, 10);
-		  	} 
-			else 
-			{
-		    	debugPrintNumber(n, base);
-		  	}
-		}
-	    void debugPrint(unsigned long n, int base = DEC)
-		{
-			if (base == 0) 
-			{
-				debugWrite(n);
-			}
-		  	else 
-			{
-				debugPrintNumber(n, base);
-			}
-		}
-	    void debugPrint(double n, int digits = 2)
-		{
-			debugPrintFloat(n, digits);
-		}	
-		void debugPrintln(void)
-		{
-			debugPrint('\r');
-			debugPrint('\n');  
-		}
-
-
 };
 
 SerialComs serialcoms;
