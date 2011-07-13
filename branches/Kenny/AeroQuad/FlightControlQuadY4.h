f10e8aa649132f2a6e404229c19c936534166eca
patricemainville@gmail.com <patricemainville@gmail.com@80d9e1a6-1c2b-11de-a108-cd2f117ce590>
2011-07-13 00:59:37 +0000

    sync with v3.0 branch

---
+++ branches/Kenny/AeroQuad/FlightControlQuadY4.h
@@ -0,0 +1,108 @@
+/*
+  AeroQuad v3.0 - April 2011
+  www.AeroQuad.com
+  Copyright (c) 2011 Ted Carancho.  All rights reserved.
+  An Open Source Arduino based multicopter.
+ 
+  This program is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation, either version 3 of the License, or 
+  (at your option) any later version. 
+ 
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+  GNU General Public License for more details. 
+ 
+  You should have received a copy of the GNU General Public License 
+  along with this program. If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+#ifndef _AQ_PROCESS_FLIGHT_CONTROL_Y4_MODE_H_
+#define _AQ_PROCESS_FLIGHT_CONTROL_Y4_MODE_H_
+
+#define FRONT_LEFT  MOTOR1
+#define REAR_1      MOTOR2
+#define FRONT_RIGHT MOTOR3
+#define REAR_2      MOTOR4
+#define LASTMOTOR   MOTOR4+1
+
+//#define YAW_DIRECTION 1 // if you want to reverse the yaw correction direction
+#define YAW_DIRECTION -1
+
+
+void applyMotorCommand() {
+  // Front = Front/Right, Back = Left/Rear, Left = Front/Left, Right = Right/Rear 
+  motors->setMotorCommand(FRONT_LEFT,  throttle - motorAxisCommandPitch + motorAxisCommandRoll);
+  motors->setMotorCommand(FRONT_RIGHT, throttle - motorAxisCommandPitch - motorAxisCommandRoll);
+  motors->setMotorCommand(REAR_2,      throttle + motorAxisCommandPitch - (YAW_DIRECTION * motorAxisCommandYaw));
+  motors->setMotorCommand(REAR_1,      throttle + motorAxisCommandPitch + (YAW_DIRECTION * motorAxisCommandYaw));
+}
+
+void processMinMaxCommand() {
+  
+  if ((motors->getMotorCommand(FRONT_LEFT) <= MINTHROTTLE) || (motors->getMotorCommand(REAR_2) <= MINTHROTTLE)){
+    delta = receiver->getData(THROTTLE) - MINTHROTTLE;
+    motorMaxCommand[FRONT_RIGHT] = constrain(receiver->getData(THROTTLE) + delta, MINTHROTTLE, MAXCHECK);
+    motorMaxCommand[REAR_1] =   constrain(receiver->getData(THROTTLE) + delta, MINTHROTTLE, MAXCHECK);
+  }
+  else if ((motors->getMotorCommand(FRONT_LEFT) >= MAXCOMMAND) || (motors->getMotorCommand(REAR_2) >= MAXCOMMAND)) {
+    delta = MAXCOMMAND - receiver->getData(THROTTLE);
+    motorMinCommand[FRONT_RIGHT] = constrain(receiver->getData(THROTTLE) - delta, MINTHROTTLE, MAXCOMMAND);
+    motorMinCommand[REAR_1]   = constrain(receiver->getData(THROTTLE) - delta, MINTHROTTLE, MAXCOMMAND);
+  }     
+  else {
+    motorMaxCommand[FRONT_RIGHT] = MAXCOMMAND;
+    motorMaxCommand[REAR_1]      = MAXCOMMAND; 
+    motorMinCommand[FRONT_RIGHT] = MINTHROTTLE;
+    motorMinCommand[REAR_1]      = MINTHROTTLE;
+  }
+
+  if ((motors->getMotorCommand(REAR_1) <= MINTHROTTLE) || (motors->getMotorCommand(FRONT_RIGHT) <= MINTHROTTLE)){
+    delta = receiver->getData(THROTTLE) - MINTHROTTLE;
+    motorMaxCommand[FRONT_LEFT] = constrain(receiver->getData(THROTTLE) + delta, MINTHROTTLE, MAXCHECK);
+    motorMaxCommand[REAR_2] = constrain(receiver->getData(THROTTLE) + delta, MINTHROTTLE, MAXCHECK);
+  }
+  else if ((motors->getMotorCommand(REAR_1) >= MAXCOMMAND) || (motors->getMotorCommand(FRONT_RIGHT) >= MAXCOMMAND)) {
+    delta = MAXCOMMAND - receiver->getData(THROTTLE);
+    motorMinCommand[FRONT_LEFT] = constrain(receiver->getData(THROTTLE) - delta, MINTHROTTLE, MAXCOMMAND);
+    motorMinCommand[REAR_2] = constrain(receiver->getData(THROTTLE) - delta, MINTHROTTLE, MAXCOMMAND);
+  }     
+  else {
+    motorMaxCommand[FRONT_LEFT] = MAXCOMMAND;
+    motorMaxCommand[REAR_2]     = MAXCOMMAND;
+    motorMinCommand[FRONT_LEFT] = MINTHROTTLE;
+    motorMinCommand[REAR_2]     = MINTHROTTLE;
+  }
+}
+
+void processHardManuevers() {
+  if (flightMode == ACRO) {
+    if (receiver->getData(ROLL) < MINCHECK) {        // Maximum Left Roll Rate
+      motorMinCommand[FRONT_RIGHT] =MAXCOMMAND;
+      motorMinCommand[REAR_1] = MAXCOMMAND;
+      motorMaxCommand[FRONT_LEFT] = minAcro;
+      motorMaxCommand[REAR_2]  = minAcro;
+    }
+    else if (receiver->getData(ROLL) > MAXCHECK) {   // Maximum Right Roll Rate
+      motorMinCommand[FRONT_LEFT]  = MAXCOMMAND;
+      motorMinCommand[REAR_2]   = MAXCOMMAND;
+      motorMaxCommand[FRONT_RIGHT] = minAcro;
+      motorMaxCommand[REAR_1]  = minAcro;
+    }
+    else if (receiver->getData(PITCH) < MINCHECK) {  // Maximum Nose Up Pitch Rate
+      motorMinCommand[FRONT_LEFT] =  MAXCOMMAND;
+      motorMinCommand[FRONT_RIGHT] = MAXCOMMAND;
+      motorMaxCommand[REAR_2]   = minAcro;
+      motorMaxCommand[REAR_1]  = minAcro;
+    }
+    else if (receiver->getData(PITCH) > MAXCHECK) {  // Maximum Nose Down Pitch Rate
+      motorMinCommand[REAR_2]   = MAXCOMMAND;
+      motorMinCommand[REAR_1]  = MAXCOMMAND;
+      motorMaxCommand[FRONT_LEFT]  = minAcro;
+      motorMaxCommand[FRONT_RIGHT] = minAcro;
+    }
+  }
+}
+
+#endif // #define _AQ_PROCESS_FLIGHT_CONTROL_X_MODE_H_
