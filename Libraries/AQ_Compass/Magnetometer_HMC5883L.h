5b2df5e61b1238ee5e6ce12fdc7b7e9e6f2f668e
patricemainville <patricemainville@gmail.com>
2011-11-26 20:00:57 +0000

    Some little adjustment

--- Libraries/AQ_Compass/Magnetometer_HMC5883L.h
+++ Libraries/AQ_Compass/Magnetometer_HMC5883L.h
@@ -93,9 +93,6 @@ void measureMagnetometer(float roll, float pitch) {
     // JI - 11/24/11 - SparkFun DOF on v2p1 Shield Configuration
     // JI - 11/24/11 - 5883L X axis points aft
     // JI - 11/24/11 - 5883L Sensor Orientation 3
-//    measuredMagX = -((Wire.receive() << 8) | Wire.receive()) * magCalibration[YAXIS];
-//    measuredMagZ = -((Wire.receive() << 8) | Wire.receive()) * magCalibration[ZAXIS];
-//    measuredMagY =  ((Wire.receive() << 8) | Wire.receive()) * magCalibration[XAXIS];
     rawMag[XAXIS] = -((Wire.receive() << 8) | Wire.receive());
 	rawMag[ZAXIS] = -((Wire.receive() << 8) | Wire.receive());
     rawMag[YAXIS] =  ((Wire.receive() << 8) | Wire.receive());
@@ -103,9 +100,6 @@ void measureMagnetometer(float roll, float pitch) {
     // JI - 11/24/11 - Sparkfun 5883L Breakout Board Upside Down on v2p0 shield
     // JI - 11/24/11 - 5883L is upside down, X axis points forward
     // JI - 11/24/11 - 5883L Sensor Orientation 5
-//    measuredMagX =  ((Wire.receive() << 8) | Wire.receive()) * magCalibration[YAXIS];
-//    measuredMagZ =  ((Wire.receive() << 8) | Wire.receive()) * magCalibration[ZAXIS];
-//    measuredMagY =  ((Wire.receive() << 8) | Wire.receive()) * magCalibration[XAXIS];
     rawMag[XAXIS] = ((Wire.receive() << 8) | Wire.receive());
 	rawMag[ZAXIS] = ((Wire.receive() << 8) | Wire.receive());
     rawMag[YAXIS] = ((Wire.receive() << 8) | Wire.receive());
