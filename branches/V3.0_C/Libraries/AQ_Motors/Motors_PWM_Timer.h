339ffbacccbb4c76c970bbb5371ee76386fe2966
patricemainville@gmail.com <patricemainville@gmail.com@80d9e1a6-1c2b-11de-a108-cd2f117ce590>
2011-12-30 14:46:59 +0000

    Some motors fix!

--- branches/V3.0_C/Libraries/AQ_Motors/Motors_PWM_Timer.h
+++ branches/V3.0_C/Libraries/AQ_Motors/Motors_PWM_Timer.h
@@ -67,40 +67,50 @@ void initializeMotors(NB_Motors numbers) {
   numberOfMotors = numbers;
 
   #if defined (__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    DDRE = DDRE | B00111000;                                  // Set ports to output PE3-5
+    DDRE = DDRE | B00111000;                                  // Set ports to output PE3-5, OC3A, OC3B, OC3C
     if (numberOfMotors == FOUR_Motors) { 
-      DDRH = DDRH | B00001000;                                // Set port to output PH3
+      DDRH = DDRH | B00001000;                                // Set port to output PH3, OC4A
     } 
     else if (numberOfMotors == SIX_Motors) {                  // for 6 
-      DDRH = DDRH | B00111000;                                // Set ports to output PH3-5
+      DDRH = DDRH | B00111000;                                // Set ports to output PH3-5, OC4A, OC4B, OC4C
     }
-    else if (numberOfMotors == EIGHT_Motors) {               // for 8 motor
-      DDRH = DDRH | B00111000;                                // Set ports to output PH3-5
-      DDRB = DDRB | B01100000;
+    else if (numberOfMotors == EIGHT_Motors) {                // for 8 motor
+      DDRH = DDRH | B00111000;                                // Set ports to output PH3-5, OC4A, OC4B, OC4C
+      DDRB = DDRB | B01100000;								  // OC1A, OC1B
     }
   #else
     DDRB = DDRB | B00001110;                                  // Set ports to output PB1-3
     DDRD = DDRD | B00001000;                                  // Set port to output PD3
   #endif
 
-  commandAllMotors(1000);                                   // Initialise motors to 1000us (stopped)
+  commandAllMotors(1000);                                     // Initialise motors to 1000us (stopped)
 
   #if defined (__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
     // Init PWM Timer 3                                       // WGMn1 WGMn2 WGMn3  = Mode 14 Fast PWM, TOP = ICRn ,Update of OCRnx at BOTTOM
     TCCR3A = (1<<WGM31)|(1<<COM3A1)|(1<<COM3B1)|(1<<COM3C1);  // Clear OCnA/OCnB/OCnC on compare match, set OCnA/OCnB/OCnC at BOTTOM (non-inverting mode)
     TCCR3B = (1<<WGM33)|(1<<WGM32)|(1<<CS31);                 // Prescaler set to 8, that gives us a resolution of 0.5us
-    ICR3 = PWM_COUNTER_PERIOD;                               // Clock_speed / ( Prescaler * desired_PWM_Frequency) #defined above.
+    ICR3 = PWM_COUNTER_PERIOD;                                // Clock_speed / ( Prescaler * desired_PWM_Frequency) #defined above.
     if (numberOfMotors == FOUR_Motors) {
       // Init PWM Timer 4
       TCCR4A = (1<<WGM41)|(1<<COM4A1);
       TCCR4B = (1<<WGM43)|(1<<WGM42)|(1<<CS41);
       ICR4 = PWM_COUNTER_PERIOD;
     }
-    else {  // for 6 or 8 motors
+    else if (numberOfMotors == SIX_Motors) {  // for 8 motors
       // Init PWM Timer 4
       TCCR4A = (1<<WGM41)|(1<<COM4A1)|(1<<COM4B1)|(1<<COM4C1);
       TCCR4B = (1<<WGM43)|(1<<WGM42)|(1<<CS41);
       ICR4 = PWM_COUNTER_PERIOD;
+    }
+	else {  // for 8 motors
+      // Init PWM Timer 4
+      TCCR4A = (1<<WGM41)|(1<<COM4A1)|(1<<COM4B1)|(1<<COM4C1);
+      TCCR4B = (1<<WGM43)|(1<<WGM42)|(1<<CS41);
+      ICR4 = PWM_COUNTER_PERIOD;
+	  // Init PWM Timer 1
+	  TCCR1A = (1<<WGM11)|(1<<COM1A1)|(1<<COM1B1);
+	  TCCR1B = (1<<WGM13)|(1<<WGM12)|(1<<CS11);
+      ICR1 = PWM_COUNTER_PERIOD;
     }
   #else
     // Init PWM Timer 1  16 bit
